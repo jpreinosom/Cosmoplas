edbcbe9e43b70bfbf8fc9e093a22b7d09b38fee4
bpmsAdmin <>
2016-05-23 16:28:01 -0400

    {/Reglas/src/main/java/cosmoplas/reglas/Firma.java}

--- Reglas/src/main/java/cosmoplas/reglas/Firma.java
+++ Reglas/src/main/java/cosmoplas/reglas/Firma.java
@@ -9,14 +9,17 @@ public class Firma implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label(value = "idMovimiento")
+   @org.kie.api.definition.type.Label("idMovimiento")
    private java.lang.Integer idMovimiento;
-   @org.kie.api.definition.type.Label(value = "idBodega")
+   @org.kie.api.definition.type.Label("idBodega")
    private java.lang.Integer idBodega;
-   @org.kie.api.definition.type.Label(value = "fechaMovimiento")
+   @org.kie.api.definition.type.Label("fechaMovimiento")
    private java.util.Date fechaMovimiento;
-   @org.kie.api.definition.type.Label(value = "validacion")
+   @org.kie.api.definition.type.Label("validacion")
    private java.lang.Boolean validacion;
+
+   @org.kie.api.definition.type.Label(value = "fechaDespacho")
+   private java.util.Date fechaDespacho;
 
    public Firma()
    {
@@ -62,13 +65,25 @@ public class Firma implements java.io.Serializable
       this.validacion = validacion;
    }
 
+   public java.util.Date getFechaDespacho()
+   {
+      return this.fechaDespacho;
+   }
+
+   public void setFechaDespacho(java.util.Date fechaDespacho)
+   {
+      this.fechaDespacho = fechaDespacho;
+   }
+
    public Firma(java.lang.Integer idMovimiento, java.lang.Integer idBodega,
-         java.util.Date fechaMovimiento, java.lang.Boolean validacion)
+         java.util.Date fechaMovimiento, java.lang.Boolean validacion,
+         java.util.Date fechaDespacho)
    {
       this.idMovimiento = idMovimiento;
       this.idBodega = idBodega;
       this.fechaMovimiento = fechaMovimiento;
       this.validacion = validacion;
+      this.fechaDespacho = fechaDespacho;
    }
 
 }
